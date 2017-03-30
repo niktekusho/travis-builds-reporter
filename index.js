f70791c168cca45df7e0957ffe0ec45bf40575f2
Nicola <nicola.dalmaso@outlook.com>
2017-03-30 12:43:43 +0200

    Added commander.js to manage input argument
    Commander.js takes care of the only (till now) argument supported by the
    script: repository name. Code refactored to embrace the new running
    option.

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const axios = require('axios');
 const TravisBuildsUtils = require('./src/TravisBuildsUtils');
 const prompt = require('prompt');
+const program = require('commander');
+
+function setupCommander() {
+  program
+    .version('1.0.0')
+    .option('-r, --repo-name <repositoryName>', 'Specify repository name')
+    .parse(process.argv);
+}
 
 // setup Travis HTTP object
 const travisHTTP = axios.create({
@@ -43,25 +51,42 @@ function fetchAllBuilds(fromBuildNumber, array, repositoryName) {
   });
 }
 
+function outputBuildsReport(builds) {
+  console.log(`Total builds count: ${TravisBuildsUtils.getBuildsCount(builds)}`);
+  console.log(`Successful builds count: ${TravisBuildsUtils.getSuccessfulBuildsCount(builds)}`);
+  console.log(`Canceled builds count: ${TravisBuildsUtils.getCanceledBuildsCount(builds)}`);
+  console.log(`Failed builds count: ${TravisBuildsUtils.getFailedBuildsCount(builds)}`);
+  console.log(`Successful builds rate: ${(TravisBuildsUtils.getSuccessfulBuildsRate(builds) * 100).toFixed(2)}%`);
+}
+
 const beginCommunication = function begin(repositoryName) {
   console.log('Fetching builds...');
   return fetchAllBuilds(null, [], repositoryName);
 };
 
 console.log('This tool returns basic builds statistics for a Travis enabled PUBLIC-ONLY repository.');
-prompt.start();
 
-prompt.get(properties, (err, result) => {
-  if (err) { return onErr(err); }
-  return beginCommunication(result.repositoryName)
+setupCommander();
+
+// TODO: refactor the following block of code to use promises or at least reduce the duplicated code
+if (program.repoName) {
+  beginCommunication(program.repoName)
   .then((builds) => {
-    console.log(`Total builds count: ${TravisBuildsUtils.getBuildsCount(builds)}`);
-    console.log(`Successful builds count: ${TravisBuildsUtils.getSuccessfulBuildsCount(builds)}`);
-    console.log(`Canceled builds count: ${TravisBuildsUtils.getCanceledBuildsCount(builds)}`);
-    console.log(`Failed builds count: ${TravisBuildsUtils.getFailedBuildsCount(builds)}`);
-    console.log(`Successful builds rate: ${(TravisBuildsUtils.getSuccessfulBuildsRate(builds) * 100).toFixed(2)}%`);
+    outputBuildsReport(builds);
   })
   .catch((error) => {
     console.log(error);
   });
-});
+} else {
+  prompt.start();
+  prompt.get(properties, (err, result) => {
+    if (err) { return onErr(err); }
+    return beginCommunication(result.repositoryName)
+    .then((builds) => {
+      outputBuildsReport(builds);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  });
+}
