bec6ad9559290a16adbc464b298e0a42e925ee5c
Nicola <nicola.dalmaso@outlook.com>
2017-04-04 14:55:24 +0200

    Refactor TravisBuildsUtilsTest

--- test/TravisBuildsUtilsTest.js
+++ test/TravisBuildsUtilsTest.js
@@ -2,34 +2,31 @@ const assert = require('assert');
 const utils = require('../src/TravisBuildsUtils.js');
 const readFile = require('fs').readFileSync;
 
-const buildsJSONFile = readFile('./test/builds.json', 'utf-8');
-const builds = JSON.parse(buildsJSONFile);
+describe('TravisBuildsUtils tests', () => {
+  const buildsCount = 10;
+  const passedCount = 9;
+  const canceledCount = 1;
+  const failedCount = 0;
 
-const buildsCount = 10;
-const passedCount = 9;
-const canceledCount = 1;
-const failedCount = 0;
+  let builds;
+  before(() => {
+    const buildsJSONFile = readFile('./test/builds.json', 'utf-8');
+    builds = JSON.parse(buildsJSONFile);
+  });
 
-describe('Total Count', () => {
-  it('should return 10', () => {
+  it('Total builds count should return 10', () => {
     assert.equal(buildsCount, utils.getBuildsCount(builds));
   });
-});
 
-describe('Passed Count', () => {
-  it('should return 9', () => {
+  it('Passed builds count should return 9', () => {
     assert.equal(passedCount, utils.getSuccessfulBuildsCount(builds));
   });
-});
 
-describe('Canceled Count', () => {
-  it('should return 1', () => {
+  it('Canceled builds count should return 1', () => {
     assert.equal(canceledCount, utils.getCanceledBuildsCount(builds));
   });
-});
 
-describe('Failed Count', () => {
-  it('should return 0', () => {
+  it('Failed builds count should return 0', () => {
     assert.equal(failedCount, utils.getFailedBuildsCount(builds));
   });
 });
