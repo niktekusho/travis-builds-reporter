4fa195fd6af344eec0ce2bdbb4c68e4a23903705
Nicola <nicola.dalmaso@outlook.com>
2017-07-05 11:37:55 +0200

    Refactor TravisBuildsUtils + Added duration basic stats

--- packages/travis-builds-reporter-core/src/builds-utils.js
+++ packages/travis-builds-reporter-core/src/builds-utils.js
@@ -2,7 +2,29 @@ const getBuildsCountForState = function getBuildsCountForState(builds, state) {
   return builds.filter(value => value.state === state).length;
 };
 
+const extractDurations = builds => builds.map(build => build.duration);
+
 module.exports = {
+
+  getMinimumBuildsDuration(builds) {
+    // build.duration is a number measured in seconds as of 05/07/2017
+    const durations = extractDurations(builds);
+    return Math.min(...durations);
+  },
+
+  getMaximumBuildsDuration(builds) {
+    const durations = extractDurations(builds);
+    return Math.max(...durations);
+  },
+
+  getAverageBuildsDuration(builds) {
+    const durations = extractDurations(builds);
+    if (durations.length > 0) {
+      return (durations.reduce((sum, newDuration) => sum + newDuration) / durations.length);
+    }
+    return NaN;
+  },
+
   getBuildsCount(builds) {
     return builds.length;
   },
