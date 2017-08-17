bf182c7590e7cbe310b468ab307d4b9d07c5d75d
Nicola <nicola.dalmaso@outlook.com>
2017-08-17 17:53:17 +0200

    Added BuildsModel class
    BuildsModel class is responsible for holding 2 pieces of information: repository name and builds.
    It also provide methods for serialization (automatically called when using JSON.stringify(obj)) and deserialization (from JSON formatted string).

--- packages/travis-builds-reporter-core/src/index.js
+++ packages/travis-builds-reporter-core/src/index.js
@@ -1,8 +1,10 @@
 const client = require('./client');
 const exporter = require('./exporter');
 const fetcher = require('./fetcher');
+const BuildsModel = require('./model');
 
 module.exports = {
+  BuildsModel,
   client,
   exporter,
   fetcher,
