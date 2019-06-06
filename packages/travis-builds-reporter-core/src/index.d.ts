d5e7d55844b49fd82c01e26413660bc0eb5359ad
niktekusho <nicola.dalmaso@outlook.com>
2019-06-06 11:26:38 +0200

    Typescript definition for core module

---
+++ packages/travis-builds-reporter-core/src/index.d.ts
@@ -0,0 +1,8 @@
+// // Type definitions for travis-builds-reporter 1.0.0
+// // Project: travis-builds-reporter
+// // Definitions by: niktekusho <https://github.com/niktekusho>
+
+export import client = require("./client");
+export import exporter = require("./exporter");
+export import fetcher = require("./fetcher");
+export import BuildsModel = require("./model");
